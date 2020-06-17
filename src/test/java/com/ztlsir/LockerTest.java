43d5cfeeeb3eaf7283a22d2e607d9599dbf64883
ztlsir <584821699@qq.com>
2020-06-17 22:08:02 +0800

    complete task should_throw_take_failed_exception_when_take_package_given_has_taken_ticket

--- src/test/java/com/ztlsir/LockerTest.java
+++ src/test/java/com/ztlsir/LockerTest.java
@@ -9,7 +9,7 @@ Given 储物柜没满 When 存包 Then 获得一张有效票据
 Given 储物柜已满 When 存包 Then 提示存包失败
 Given 一张有效票据 When 取包 Then 取包成功
 Given 一张伪造票据 When 取包 Then 取包失败，提示非法票据
-todo:Given 一张已取过包的票据 When 取包 Then 取包失败，提示非法票据
+Given 一张已取过包的票据 When 取包 Then 取包失败，提示非法票据
 */
 public class LockerTest {
 
@@ -56,6 +56,20 @@ public class LockerTest {
         Exception exception = assertThrows(
                 RuntimeException.class,
                 () -> locker.takePackage("fake_ticket"));
+
+        assertEquals(ilLegalTicketErrorMessage, exception.getMessage());
+    }
+
+    @Test
+    public void should_throw_take_failed_exception_when_take_package_given_has_taken_ticket()
+    {
+        Locker locker=new Locker(false);
+        String ticket=locker.savePackage();
+        locker.takePackage(ticket);
+
+        Exception exception = assertThrows(
+                RuntimeException.class,
+                () -> locker.takePackage(ticket));
 
         assertEquals(ilLegalTicketErrorMessage, exception.getMessage());
     }
