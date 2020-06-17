9916296e5f5b85b87e6ccb8eb3a410b392831ed6
ztlsir <584821699@qq.com>
2020-06-17 22:16:47 +0800

    change task 'Given 一张有效票据 When 取包 Then 取包成功' to 'Given 一张有效票据 When 取包 Then 成功取到票据对应的包'

--- src/test/java/com/ztlsir/LockerTest.java
+++ src/test/java/com/ztlsir/LockerTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 /*
 Given 储物柜没满 When 存包 Then 获得一张有效票据
 Given 储物柜已满 When 存包 Then 提示存包失败
-Given 一张有效票据 When 取包 Then 取包成功
+Given 一张有效票据 When 取包 Then 成功取到票据对应的包
 Given 一张伪造票据 When 取包 Then 取包失败，提示非法票据
 Given 一张已取过包的票据 When 取包 Then 取包失败，提示非法票据
 */
