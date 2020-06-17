5d25407a6b1007dd2157aa991955d5a58de9027c
ztlsir <584821699@qq.com>
2020-06-17 18:26:55 +0800

    complete task should_return_ticket_when_save_package_given_locker_is_not_full

---
+++ src/main/java/com/ztlsir/Locker.java
@@ -0,0 +1,11 @@
+package com.ztlsir;
+
+public class Locker {
+    public Locker(boolean b) {
+
+    }
+
+    public String savePackage() {
+        return "1";
+    }
+}
--- src/test/java/com/ztlsir/LockerTest.java
+++ src/test/java/com/ztlsir/LockerTest.java
@@ -1,4 +1,25 @@
 package com.ztlsir;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+Given 储物柜没满 When 存包 Then 获得一张有效票据
+todo:Given 储物柜已满 When 存包 Then 提示存包失败
+todo:Given 一张有效票据 When 取包 Then 取包成功
+todo:Given 一张伪造票据 When 取包 Then 取包失败，提示非法票据
+todo:Given 一张已取过包的票据 When 取包 Then 取包失败，提示非法票据
+*/
 public class LockerTest {
+    @Test
+    public void should_return_ticket_when_save_package_given_locker_is_not_full()
+    {
+        Locker locker=new Locker(false);
+
+        String ticket=locker.savePackage();
+
+        assertNotNull(ticket);
+        assertNotEquals("",ticket);
+    }
 }
