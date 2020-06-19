ddff49866ce98cad58a19cd58bece91e49658ded
ztlsir <584821699@qq.com>
2020-06-19 16:14:33 +0800

    complete:Given SmartLockerRoot和PrimaryLockerRoot共同管理2个储物柜，第1个储物柜空余量为5，第2个储物柜空余量为4 When 存包 Then 获得一张有效票据，包存到第1个储物柜

--- src/test/java/com/ztlsir/SmartLockerRootTest.java
+++ src/test/java/com/ztlsir/SmartLockerRootTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Given SmartLockerRoot和PrimaryLockerRoot共同管理2个储物柜，第1个储物柜空余量为5，第2个储物柜空余量为5 When 存包 Then 获得一张有效票据，包存到第1个储物柜
- *todo Given SmartLockerRoot和PrimaryLockerRoot共同管理2个储物柜，第1个储物柜空余量为5，第2个储物柜空余量为4 When 存包 Then 获得一张有效票据，包存到第1个储物柜
+ * Given SmartLockerRoot和PrimaryLockerRoot共同管理2个储物柜，第1个储物柜空余量为5，第2个储物柜空余量为4 When 存包 Then 获得一张有效票据，包存到第1个储物柜
  *todo Given SmartLockerRoot和PrimaryLockerRoot共同管理2个储物柜，第1个储物柜空余量为4，第2个储物柜空余量为5 When 存包 Then 获得一张有效票据，包存到第2个储物柜
  *todo Given SmartLockerRoot和PrimaryLockerRoot共同管理2个储物柜，第1个储物柜空余量为0，第2个储物柜空余量为5 When 存包 Then 获得一张有效票据，包存到第2个储物柜
  *todo Given SmartLockerRoot和PrimaryLockerRoot共同管理2个储物柜，第1个储物柜空余量为5，第2个储物柜空余量为0 When 存包 Then 获得一张有效票据，包存到第1个储物柜
@@ -24,6 +24,19 @@ public class SmartLockerRootTest {
     public void should_save_in_1st_locker_and_return_ticket_when_save_package_given_smart_and_primary_locker_robot_manage_two_lockers_and_1st_locker_with_5_and_2nd_with_5() {
         Locker firstLocker = new Locker(5);
         SmartLockerRoot smartLockerRoot = new SmartLockerRoot(Arrays.asList(firstLocker, new Locker(5)));
+        Pack preSavePack = new Pack();
+
+        Ticket ticket = smartLockerRoot.savePackage(preSavePack);
+
+        assertTicketNotEmpty(ticket);
+        Pack pack = firstLocker.takePackage(ticket);
+        assertEquals(preSavePack, pack);
+    }
+
+    @Test
+    public void should_save_in_1st_locker_and_return_ticket_when_save_package_given_smart_and_primary_locker_robot_manage_two_lockers_and_1st_locker_with_5_and_2nd_with_4() {
+        Locker firstLocker = new Locker(5);
+        SmartLockerRoot smartLockerRoot = new SmartLockerRoot(Arrays.asList(firstLocker, new Locker(4)));
         Pack preSavePack = new Pack();
 
         Ticket ticket = smartLockerRoot.savePackage(preSavePack);
