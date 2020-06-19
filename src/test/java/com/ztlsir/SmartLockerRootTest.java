de36f55d158e2b79e5fee914d9e57cbf91807172
ztlsir <584821699@qq.com>
2020-06-19 22:51:23 +0800

    add task to SmartLockerRoot

--- src/test/java/com/ztlsir/SmartLockerRootTest.java
+++ src/test/java/com/ztlsir/SmartLockerRootTest.java
@@ -22,6 +22,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * Given SmartLockerRoot和PrimaryLockerRoot共同管理2个储物柜，一张伪造票据 When 通过SmartLockerRoot取包 Then 取包失败，提示非法票据
  * Given SmartLockerRoot和PrimaryLockerRoot共同管理2个储物柜，通过PrimaryLockerRoot存包取得的一张有效票据 When 通过SmartLockerRoot取包 Then 取包成功
  * Given SmartLockerRoot和PrimaryLockerRoot共同管理2个储物柜，通过SmartLockerRoot存包取得的一张有效票据 When 通过PrimaryLockerRoot取包 Then 取包成功
+ * todo Given SmartLockerRoot和PrimaryLockerRoot共同管理2个储物柜，第1个储物柜空余量为1，第2个储物柜空余量为0 When 存包 Then 获得一张有效票据，包存到第1个储物柜，第1个储物柜存满
+ * todo Given SmartLockerRoot和PrimaryLockerRoot共同管理2个储物柜，第1个储物柜空余量为0，第2个储物柜空余量为0，通过SmartLockerRoot存包到第1个储物柜取得的一张有效票据 When 通过SmartLockerRoot取包 Then 取包成功，第1个储物柜有余量
  */
 public class SmartLockerRootTest {
     @Test
