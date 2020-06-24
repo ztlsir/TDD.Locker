92d669217064df987b713deacab88d19a4ee00ef
ztlsir <584821699@qq.com>
2020-06-24 14:50:35 +0800

    complete:Given LockerRobotManager管理着2个有容量的机器人，第1个机器人为PrimaryLockerRobot，第2个机器人为SmartLockerRobot，一张包存在第1个机器人的有效票 When 取包 Then 取包成功

--- src/test/java/com/ztlsir/LockerRobotManagerTest.java
+++ src/test/java/com/ztlsir/LockerRobotManagerTest.java
@@ -77,7 +77,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * When 取包
  * Then 取包成功
  * <p>
- * todo Given LockerRobotManager管理着2个有容量的机器人，第1个机器人为PrimaryLockerRobot，第2个机器人为SmartLockerRobot，一张包存在第1个机器人的有效票
+ * done Given LockerRobotManager管理着2个有容量的机器人，第1个机器人为PrimaryLockerRobot，第2个机器人为SmartLockerRobot，一张包存在第1个机器人的有效票
  * When 取包
  * Then 取包成功
  * <p>
@@ -294,6 +294,21 @@ public class LockerRobotManagerTest {
                 lockers);
         Pack preSavePack = new Pack();
         Ticket ticket = lockers.get(1).savePackage(preSavePack);
+
+        Pack pack = manager.takePackage(new Ticket(ticket.getSerialNo()));
+
+        assertEquals(preSavePack, pack);
+    }
+
+    @Test
+    public void should_take_package_by_ticket_when_take_package_given_locker_robot_manager_manage_two_available_robot_and_one_valid_ticket_from_1st_robot_that_locker_robot_is_primary_and_smart() {
+        List<Locker> primaryAvailableLockers = createAvailableLockers(1);
+        LockerRobotManager manager = new LockerRobotManager(
+                Arrays.asList(
+                        new PrimaryLockerRobot(primaryAvailableLockers),
+                        new SmartLockerRobot(createAvailableLockers(1))));
+        Pack preSavePack = new Pack();
+        Ticket ticket = primaryAvailableLockers.get(0).savePackage(preSavePack);
 
         Pack pack = manager.takePackage(new Ticket(ticket.getSerialNo()));
 
